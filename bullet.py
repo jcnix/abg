8b83d9a9b329423437fc2a74f86812bee4ac6031
Casey Jones <jonescaseyb@gmail.com>
2009-07-25 20:57:45 -0500

    Once bullet is off screen, delete it.

--- bullet.py
+++ bullet.py
@@ -12,14 +12,17 @@ class Bullet:
         self.bulletrect.move_ip(pshiprect)
         self.bulletrects.append(self.bulletrect)
                 
-    def move(self, screen):       
+    def move(self, screen):
+        to_delete = []
+        
         for i in range(len(self.bulletrects)):
             self.bulletrects[i] = self.bulletrects[i].move(self.bullet_speed)
             screen.blit(self.bullet, self.bulletrects[i])
             
             #If bullet goes off the top of the screen
             if self.bulletrects[i].top < 0:
-                pass
-                #self.bulletrects.remove(self.bulletrects[i])
-                #del self.bulletrects[i];
+                to_delete.append(self.bulletrects[i])
+        
+        for x in range(len(to_delete)):
+            del self.bulletrects[x]
                 

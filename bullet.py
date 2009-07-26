784b95f508de24c2d7ee3c87afb54767d9bd46f6
Casey Jones <jonescaseyb@gmail.com>
2009-07-25 20:22:59 -0500

    Bullets now fire from correct position

--- bullet.py
+++ bullet.py
@@ -2,28 +2,24 @@
 import sys, pygame
 
 class Bullet:
-    is_firing = False
     bullet = pygame.image.load("res/bullet.png").convert()
     bulletrect = bullet.get_rect()
     bullet_speed = [0, -3]
     bulletrects = []
-    #bullets = []
 
-    def fire(self, pshiprect):
-        #self.bullets.append(self.bullet)
-        self.bulletrects.append(self.bulletrect)
-        
+    def fire(self, pshiprect):       
         self.bulletrect = self.bullet.get_rect()
         self.bulletrect.move_ip(pshiprect)
-        self.is_firing = True
+        self.bulletrects.append(self.bulletrect)
                 
     def move(self, screen):       
-        #if self.is_firing:
         for i in range(len(self.bulletrects)):
             self.bulletrects[i] = self.bulletrects[i].move(self.bullet_speed)
             screen.blit(self.bullet, self.bulletrects[i])
             
             #If bullet goes off the top of the screen
-            if self.bulletrect.top < 0:
-                self.is_firing = False
-    
+            if self.bulletrects[i].top < 0:
+                pass
+                #self.bulletrects.remove(self.bulletrects[i])
+                #del self.bulletrects[i];
+                

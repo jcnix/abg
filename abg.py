8466785b6cdddb1101112538289b7c4a22f65d93
Casey Jones <jonescaseyb@gmail.com>
2009-07-25 20:07:14 -0500

    Fire multiple bullets

--- abg.py
+++ abg.py
@@ -35,7 +35,6 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                print "fire"
                 bullet.fire(pshiprect.midtop)
     
     screen.fill(black)
--- bullet.py
+++ bullet.py
@@ -6,19 +6,22 @@ class Bullet:
     bullet = pygame.image.load("res/bullet.png").convert()
     bulletrect = bullet.get_rect()
     bullet_speed = [0, -3]
+    bulletrects = []
+    #bullets = []
 
     def fire(self, pshiprect):
-        self.bullet = pygame.image.load("res/bullet.png").convert()
+        #self.bullets.append(self.bullet)
+        self.bulletrects.append(self.bulletrect)
+        
         self.bulletrect = self.bullet.get_rect()
         self.bulletrect.move_ip(pshiprect)
         self.is_firing = True
                 
     def move(self, screen):       
-        if self.is_firing:
-            self.bulletrect = self.bulletrect.move(self.bullet_speed)
-            screen.blit(self.bullet, self.bulletrect)
-            #pygame.display.flip()
-            print self.bulletrect.top
+        #if self.is_firing:
+        for i in range(len(self.bulletrects)):
+            self.bulletrects[i] = self.bulletrects[i].move(self.bullet_speed)
+            screen.blit(self.bullet, self.bulletrects[i])
             
             #If bullet goes off the top of the screen
             if self.bulletrect.top < 0:

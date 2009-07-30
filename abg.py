4392081c8c5a0952b404326b7069ff655903431f
Casey Jones <jonescaseyb@gmail.com>
2009-07-29 19:30:43 -0500

    Made left/right movement depend on render time
    This makes it so movement should be the same speed no matter how many frames are
    rendered per second.

--- abg.py
+++ abg.py
@@ -1,18 +1,17 @@
 # -*- coding: iso-8859-1 -*-
 import sys, pygame
+from time import time
+
 pygame.init()
 
 size = width, height = 800, 600
-move_right = [1, 0]
-move_left = [-1, 0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
 from bullet import Bullet
-bullet = Bullet()
-
 from enemy import Enemy
+bullet = Bullet()
 enemy = Enemy();
 
 pygame.display.set_caption("Alpha Beta Gamma")
@@ -22,8 +21,12 @@ pship = pygame.image.load("res/player_ship.png").convert()
 pshiprect = pship.get_rect()
 
 pshiprect.move_ip(width/2, height - 25)
+current_time = time()
+end_time = current_time
 
 while 1:
+    move_right = [1.5, 0]
+    move_left = [-1.5, 0]
     pygame.event.pump()
     key = pygame.key.get_pressed()
     
@@ -48,3 +51,11 @@ while 1:
     bullet.move(screen, enemies)
     
     pygame.display.flip()
+
+    #find how long it took to render so we can adjust speeds
+    end_time = time()
+    diff_time = end_time - current_time
+    current_time = end_time
+    
+    move_right = [diff_time*x for x in move_right]
+    move_left = [diff_time*x for x in move_left]

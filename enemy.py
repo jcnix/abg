4dc2325330ba254475cffc9e0332b949822454c7
Casey Jones <jonescaseyb@gmail.com>
2009-08-04 20:48:44 -0500

    GPL v3 License.

--- enemy.py
+++ enemy.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+
+# File:   enemy.py
+# Author: Casey Jones
+#
+# Created on July 20, 2009, 4:48 PM
+#
+# This file is part of Alpha Beta Gamma (abg).
+#
+# ABG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ABG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ABG.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import sys, pygame, frametime
 import random
 

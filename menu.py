e88f9979286eef1f19ca8b0fd85cc5124c0d2d33
Casey Jones <jonescaseyb@gmail.com>
2009-08-19 20:18:06 -0500

    Display a banner in the menu.

--- menu.py
+++ menu.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import pygame
+import pygame, properties
 
 class Menu:
     screen = None
+    banner = pygame.image.load("res/abgbanner.png").convert()
+    bannerrect = banner.get_rect()
+    blackSurface = pygame.Surface([banner.get_width(), banner.get_height()])
+    blackSurface.fill([0,0,0])
     
     def __init__(self, screen):
         self.screen = screen
     
     def showMenu(self):
         inMenu = True
+        self.screen.blit(self.banner, self.bannerrect)     
+        pygame.display.update(self.bannerrect)
         while inMenu:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:
+                        self.screen.blit(self.blackSurface, self.bannerrect)     
+                        pygame.display.update(self.bannerrect)                 
                         inMenu = False
